137d8c3d0554b6ac0c31c3ccb8aa2066d45791e7
ivyteew <danilotoro112@gmail.com>
2021-11-17 13:20:43 -0500

    Mejoras en la estructura

--- evaluaciones/configurarEvaluacion/script.js
+++ evaluaciones/configurarEvaluacion/script.js
@@ -18,3 +18,19 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+	optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+		o.addEventListener("click", () => {
+		selected.innerHTML = o.querySelector("label").innerHTML;
+		optionsContainer.classList.remove("active");
+	});
+});
--- evaluaciones/crearEvaluación/script.js
+++ evaluaciones/crearEvaluación/script.js
@@ -173,3 +173,21 @@ optionsList.forEach(o => {
         add_question_container(o.querySelector("label").innerHTML);
     });
 });
+
+window.onscroll = function(){
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop > 100) {
+      document.querySelector('.go-top-container').classList.add('show');
+       
+    }
+    else{
+      document.querySelector('.go-top-container').classList.remove('show');
+    }
+  }
+   
+  document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
